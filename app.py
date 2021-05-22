7a70c39e5a57d09bd92559af1a86462fb43cb940
kjwon15 <kjwonmail@gmail.com>
2021-05-23 01:22:07 +0900

    Add delete function

--- app.py
+++ app.py
@@ -34,3 +34,13 @@ def get_file(fname):
     return send_from_directory(
         app.config['UPLOAD_DIR'], fname,
         attachment_filename=fname)
+
+
+@app.delete('/<fname>')
+def delete_file(fname):
+    try:
+        os.remove(os.path.join(app.config['UPLOAD_DIR'], fname))
+    except FileNotFoundError:
+        return '', 404
+
+    return 'OK'
